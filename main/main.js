bc76698144f0a7979f7bfe515afd3bb9268a72dc
lifeng9980 <lifeng9980@gmail.com>
2019-05-31 13:32:46 +0800

    updates using if/switch

--- main/main.js
+++ main/main.js
@@ -1,4 +1,24 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+	//var result = "";
+	//if (str == "910"){
+	//	result = "._. ... ._.\n"+"|_| ..| |.|\n"+"..| ..| |_|\n";
+	//}
+	//else if (str == "256"){
+	//	result = "._. ._. ._.\n"+"._| |_. |_.\n"+"|_. ._| |_|\n";
+	//}
+	//else if (str == "7"){
+	//	result = "._.\n"+"..|\n"+"..|\n";
+	//}
+	//return result;
+	switch(str){
+		case "910":
+			return "._. ... ._.\n"+"|_| ..| |.|\n"+"..| ..| |_|\n";
+			break;
+		case "256":
+			return "._. ._. ._.\n"+"._| |_. |_.\n"+"|_. ._| |_|\n";
+			break;
+		case "7":
+			return "._.\n"+"..|\n"+"..|\n";
+			break;
+	}
 };
